a6873b93ae693d7ab28097f15507cac8655f40c0
GaSiqueira <125694331+GaSiqueira@users.noreply.github.com>
2023-11-15 12:19:05 -0300

    Alterações atv3

---
+++ src/componentes/inicio.js
@@ -0,0 +1,8 @@
+export default function Inicio(){
+    return(
+        <div className="container-fluid">
+            <h2>Interface PetLovers</h2>
+            <p>Projeto desenvolvido para ter uma base visual para o sistema do PetLovers</p>
+        </div>
+    )
+}
--- src/componentes/roteador.js
+++ src/componentes/roteador.js
@@ -2,28 +2,37 @@
 import { useState } from "react";
 import BarraNavegacao from "./barraNavegacao"
 import ListaCliente from "./listaCliente";
-import FormularioCadastroCliente from "./formularioCadastroCliente";
+import FormularioCadastroCliente from "./cadastros/formularioCadastroCliente";
+import Inicio from "./inicio";
 
 export default function Roteador() {
-    const [tela, setTela] = useState('Clientes')
+    const [tela, setTela] = useState('Cadastros')
     const selecionarView = (valor, e) => {
         e.preventDefault()
         setTela(valor)
     }
 
     const construirView = () => {
-        if (tela === 'Clientes') {
+        if (tela === 'Listar') {
             return (
                 <>
-                    <BarraNavegacao seletorView={selecionarView} tema="#e3f2fd" botoes={['Clientes', 'Cadastros']} />
+                    <BarraNavegacao seletorView={selecionarView} tema="#e3f2fd" botoes={['Clientes', 'Pets', 'Serviços', 'Produtos']} />
                     <ListaCliente tema="#e3f2fd" />
                 </>
             )
-        } else {
+        } if (tela === 'Cadastros') {
             return (
                 <>
-                    <BarraNavegacao seletorView={selecionarView} tema="#e3f2fd" botoes={['Clientes', 'Cadastros']} />
+                    <BarraNavegacao seletorView={selecionarView} tema="#e3f2fd" botoes={['inicio', 'Clientes', 'Pet', 'Serviço', 'Produto']} />
                     <FormularioCadastroCliente tema="#e3f2fd" />
+                </>
+            )
+        }
+        else{
+            return(
+                <>
+                    <BarraNavegacao seletorView={selecionarView} tema="#e3f2fd" botoes ={['Listar', 'Cadastros']} />
+                    <Inicio tema='#e3f2fd'/>
                 </>
             )
         }
