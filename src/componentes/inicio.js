2a93c0c373b1bd45a3445a09e2ad30f9d114d82a
GaSiqueira <125694331+GaSiqueira@users.noreply.github.com>
2023-12-02 21:28:04 -0300

    Att atv3

--- src/componentes/inicio.js
+++ src/componentes/inicio.js
@@ -2,7 +2,7 @@ export default function Inicio(){
     return(
         <div className="container-fluid">
             <h2>Interface PetLovers</h2>
-            <p>Projeto desenvolvido para ter uma base visual para o sistema do PetLovers</p>
+            <p>Projeto desenvolvido para ter uma base visual para o sistema do PetLovers, a Interface é a mesma utilizada no proejeto passado, só que utiizando HOOKS!</p>
         </div>
     )
 }
